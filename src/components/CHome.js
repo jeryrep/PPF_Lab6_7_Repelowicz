20a9b3721aaa38c09e6599cb3ef0ce15165d5069
Jerzy <jeryrep@gmail.com>
2021-01-04 19:41:30 +0100

    Obsługa formularza

--- src/components/CHome.js
+++ src/components/CHome.js
@@ -1,8 +1,14 @@
 import React from "react";
-import {CVideoArticle} from "./CVideoArticle";
 
 export const CHome = () => {
     return (
-        <CVideoArticle/>
+        <article className="videoContent">
+            <p className="videoText text-warning">
+                Od wielu lat zajmujemy się wszelkiego rodzaju metalami. Chcieliśmy zaprezentować film, który przybliży Ci, o czym jest nasza strona.
+            </p>
+            <div>
+                <iframe src="https://www.youtube.com/embed/j0KwZZvOBCI" className="video" allowFullScreen title="Jak produkuje się stal?"/>
+            </div>
+        </article>
     );
 }
